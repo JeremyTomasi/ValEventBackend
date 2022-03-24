0db88d137fde237851af777ed13aaf71f36f9d18
Jeremy Tomasi <lugurbaldev@outlook.com>
2022-03-24 09:38:48 +0100

    Ajout des différentes repositories

---
+++ src/main/java/fr/jtomasi/feveventbackend/repository/AssoRepository.java
@@ -0,0 +1,7 @@
+package fr.jtomasi.feveventbackend.repository;
+
+import fr.jtomasi.feveventbackend.model.Asso;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AssoRepository extends CrudRepository<Asso,Integer> {
+}
---
+++ src/main/java/fr/jtomasi/feveventbackend/repository/EventLikedRepository.java
@@ -0,0 +1,7 @@
+package fr.jtomasi.feveventbackend.repository;
+
+import fr.jtomasi.feveventbackend.model.EventLiked;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EventLikedRepository extends CrudRepository<EventLiked,Integer> {
+}
