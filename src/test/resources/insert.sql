7daceae2b741baa96c77249cb6e827e4e3400d31
Jeremy Tomasi <lugurbaldev@outlook.com>
2022-03-24 11:09:27 +0100

    Mise en place de l'environnement de test

---
+++ src/test/resources/insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO assos(id,name,email,profile_pic_link,description) VALUES ('1','SPHIMX','sphimx@sphimx.com','/test.png','Association des etudiants en sciences');
+
+INSERT INTO events(id,name,date,description,poster_url,asso_organizer_id1) VALUES ('1','Congrès FES','18/02/2022','Congrès Femmes en Sciences','/congres_fes.png',1);
+
+INSERT INTO users(id, email, name, profile_pic_link, first_name) VALUES ('1','jeremy.tomasi@outlook.com','Tomasi','/test.png','Jeremy');
