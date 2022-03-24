782a3b74767c5ef6cb8764baaebc0876536c4ca1
Jeremy Tomasi <lugurbaldev@outlook.com>
2022-03-24 09:24:50 +0100

    Premier commit

---
+++ src/main/java/fr/jtomasi/feveventbackend/FevEventBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.jtomasi.feveventbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FevEventBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FevEventBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/jtomasi/feveventbackend/FevEventBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.jtomasi.feveventbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FevEventBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
